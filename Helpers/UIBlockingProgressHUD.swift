811d9b5edb4ba13f083672782276ae26381956ce
2underground <m.andriyuk@gmail.com>
2023-10-03 00:17:26 +0300

    0=0

--- Helpers/UIBlockingProgressHUD.swift
+++ Helpers/UIBlockingProgressHUD.swift
@@ -6,23 +6,15 @@ import UIKit
 import ProgressHUD
 
 final class UIBlockingProgressHUD {
-    static let shared = UIBlockingProgressHUD()
-    
     private static var window: UIWindow? {
         return UIApplication.shared.windows.first
     }
-    
     static func show() {
-        DispatchQueue.main.async {
-            window?.isUserInteractionEnabled = false
-            ProgressHUD.show()
-        }
+        window?.isUserInteractionEnabled = false
+        ProgressHUD.show()
     }
-    
     static func dismiss() {
-        DispatchQueue.main.async {
-            window?.isUserInteractionEnabled = true
-            ProgressHUD.dismiss()
-        }
+        window?.isUserInteractionEnabled = true
+        ProgressHUD.dismiss()
     }
 }

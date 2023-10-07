6c7b8a84530a5b88a1e4f02e7072296263373c3b
2underground <m.andriyuk@gmail.com>
2023-10-07 17:57:15 +0300

    0=0

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -1,6 +1,8 @@
 import WebKit
 import UIKit
 
+//
+
 protocol WebViewViewControllerDelegate: AnyObject {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
     func webViewViewControllerDidCancel(_ vc: WebViewViewController)
